4717115974908b5d2543ff35517e3c31f2937b01
shubhank <shubhank.vijayvergiya@gmail.com>
2020-11-09 23:31:45 +1100

    [fix] table DDL

--- setup/postgres/create_user_purchase.sql
+++ setup/postgres/create_user_purchase.sql
@@ -1,15 +1,21 @@
+drop schema if exists retail cascade ;
 CREATE SCHEMA retail;
 
+drop table if exists retail.user_purchase cascade;
 CREATE TABLE retail.user_purchase (
-    invoice_number varchar(10),
-    stock_code varchar(20),
+    invoice_number varchar(500),
+    stock_code varchar(500),
     detail varchar(1000),
     quantity int,
     invoice_date timestamp,
     unit_price Numeric(8,3),
     customer_id int,
-    country varchar(20)
+    country varchar(500)
 );
 
-COPY retail.user_purchase(invoice_number,stock_code,detail,quantity,invoice_date,unit_price,customer_id,country) 
-FROM '/data/retail/OnlineRetail.csv' DELIMITER ','  CSV HEADER;
+--  \copy retail.user_purchase from 'setup/raw_input_data/retail/OnlineRetail.csv' delimiter ',' csv header;
+COPY retail.user_purchase(invoice_number,
+stock_code,detail,quantity,
+invoice_date,unit_price,customer_id,country)
+FROM 'setup/raw_input_data/retail/OnlineRetail.csv'
+DELIMITER ','  CSV HEADER;
