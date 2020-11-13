4385303d6ae72de7ef0cf0bbf664241f7c93ae2e
shubhank <shubhank.vijayvergiya@gmail.com>
2020-11-14 00:53:42 +1100

    chore

--- dags/user_behaviour.py
+++ dags/user_behaviour.py
@@ -3,11 +3,19 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 
+from airflow.operators.postgres_operator import PostgresOperator
+
+# config
+# local
+unload_user_purchase = './scripts/sql/filter_unload_user_purchase.sql'
+temp_filtered_user_purchase = '/temp/temp_filtered_user_purchase.csv'
+
 default_args = {
     "owner": "airflow",
     "depends_on_past": True,
     'wait_for_downstream': True,
-    "start_date": datetime(2010, 12, 1), # we start at this date to be consistent with the dataset we have and airflow will catchup
+    "start_date": datetime(2010, 12, 1),
+    # we start at this date to be consistent with the dataset we have and airflow will catchup
     "email": ["airflow@airflow.com"],
     "email_on_failure": False,
     "email_on_retry": False,
@@ -19,5 +27,17 @@ dag = DAG("user_behaviour", default_args=default_args,
           schedule_interval="0 0 * * *", max_active_runs=1)
 
 end_of_data_pipeline = DummyOperator(task_id='end_of_data_pipelines', dag=dag)
+
+pg_unload = PostgresOperator(
+    dag=dag,
+    task_id='pg_unload',
+    sql=unload_user_purchase,
+    postgres_conn_id='postgres_default',
+    params={'temp_filtered_user_purchase': temp_filtered_user_purchase},
+    depends_on_past=True,
+    wait_for_downstream=True
+)
+
+pg_unload >> end_of_data_pipeline
 
 end_of_data_pipeline
